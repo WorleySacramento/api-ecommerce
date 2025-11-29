60a10f52109c8419e3b80716a54e8493002626dc
Worley Sacramento <78088577+WorleySacramento@users.noreply.github.com>
2025-11-29 10:01:48 -0300

    feat: refatorar middleware de autenticação para uso assíncrono e melhorar tratamento de token

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,17 +1,23 @@
 import { NextFunction, Request, Response } from 'express';
-import  express  from 'express';
+import express from 'express';
 import { UnauthorizedError } from '../errors/unauthorized.error';
+import { getAuth } from 'firebase-admin/auth';
 
 
 
 export const auth = (app: express.Express) => {
-  app.use((req: Request, res: Response, next: NextFunction) => {
-   const token = req.headers.authorization?.split('Bearer ')[1]; 
+  app.use(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split('Bearer ')[1];
 
-   if(token) {
-    console.log('Token recebido:', token);
-   return next();
-   }
+    if (token) {
+      try {
+        const decodeIdToken = await getAuth().verifyIdToken(token, true)
+        console.log('decodeIdToken', decodeIdToken);
+        return next();
+      } catch (error) {
+        next(new UnauthorizedError());
+      }
+    }
     next(new UnauthorizedError());
   });
 }
