62cc1426fbc447a579118dbdbc0bd7d40b0fabcd
Worley Sacramento <78088577+WorleySacramento@users.noreply.github.com>
2025-12-04 18:52:24 -0300

    feat: atualizar esquema de validação de empresa e rotas para incluir novos campos

--- src/models/company.model.ts
+++ src/models/company.model.ts
@@ -17,7 +17,7 @@ export type Company = {
 };
 
 
-export const companySchema = Joi.object().keys({
+export const newCompanySchema = Joi.object().keys({
   razaoSocial: Joi.string().required(),
   nomeFantasia: Joi.string().required(),
   logoMarca: Joi.string().allow(null),
@@ -34,10 +34,19 @@ export const companySchema = Joi.object().keys({
 })
 
 export const updateCompanySchema = Joi.object().keys({
-  name: Joi.string().required(),
-  idade: Joi.number().integer().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).optional()
+  razaoSocial: Joi.string().required(),
+  nomeFantasia: Joi.string().required(),
+  logoMarca: Joi.string().allow(null),
+  cpfCnpj: Joi.alternatives().try(
+    Joi.string().length(11).required(),
+    Joi.string().length(14).required()
+  ),
+  telefone: Joi.string().regex(/^(\(?\d{2}\)?\s?)?9?\d{4}-?\d{4}$/).required(),
+  horarioFuncionamento: Joi.string().required(),
+  endereco: Joi.string().required(),
+  localizacao: Joi.string().required(),
+  taxaEntrega: Joi.number().required(),
+  ativa: Joi.boolean().required(),
 })
 
 
